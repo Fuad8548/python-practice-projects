9cbd54f4314782b01d85570f0f05a934e21c5984
Al Fuad <alfuad8548@gmail.com>
2026-07-25 04:35:08 +0000

    player move direction fixed

--- maze_walker.py
+++ maze_walker.py
@@ -1,48 +1,59 @@
 maze = [
     ["W", "W", "W", "W", "W"],
-    ["W", ".", ".", ".", "W"],
+    [".", ".", ".", ".", "W"],
     ["W", ".", "W", ".", "W"],
     ["W", ".", ".", "G", "W"],
     ["W", "W", "W", "W", "W"]
 ]
 
-player_pos = (0, 4)
+player_pos = (0, 0)
+move_history = []
 
-for row in enumerate(maze):
+for i, row in enumerate(maze):
+    print(f"Row: {i}", end = " ")
     for cell in row:
-        while True:
-            direction = input("Which way: (Up/ Down/ Left/ Right/ Quit):")
+        print(cell, end = " ")
+    print()
+        
+while True:
+    direction = input("Which way: (Up/ Down/ Left/ Right/ Quit):")
 
-            if direction == "Quit":
-                print("Thanks for playing!")
-                break
+    if direction == "Quit":
+        print("Thanks for playing!")
+        break
 
-            if direction == "Up":
-                delta = (-1, 0)
-            elif direction == "Down":
-                delta = (1, 0)
-            elif direction == "Left":
-                delta = (0, -1)
-            elif direction == "Right":
-                delta = (0, 1)         
-            else:
-                print("Not a valid direction! Try again")
-                continue
+    if direction == "Up":
+        delta = (-1, 0)
+    elif direction == "Down":
+        delta = (1, 0)
+    elif direction == "Left":
+        delta = (0, -1)
+    elif direction == "Right":
+        delta = (0, 1)         
+    else:
+        print("Not a valid direction! Try again")
+        continue
 
-            new_row = player_pos[0] + delta[0]
-            new_col = player_pos[1] + delta[1]
+    new_row = player_pos[0] + delta[0]
+    new_col = player_pos[1] + delta[1]
 
-            if new_row < 0 or 0 <= new_col and new_col < len(maze[0]):
-                print("Nay! Out of the border!")
-            elif maze[new_row][new_col] == "W":
-                print("There is wall, try a different direction")
+    if new_row < 0 or new_col >= len(row):
+        print("Nay! Out of the border!")
+        continue  
+    elif maze[new_row][new_col] == "W":
+        print("There is wall, try a different direction")
+        continue
 
-            player_pos = (new_row, new_col)
-            print("Player moved", direction, ": at", player_pos)
+    player_pos = (new_row, new_col)
+    move_history.append(direction)
+    print("Player moved", direction, ": at", player_pos)
     
-            if maze[new_row][new_col] == "G":
-                print("Voila! you reached the goal!")
-                break 
+    if maze[new_row][new_col] == "G":
+        print("Voila! you reached the goal!")
+        break 
+    
+print("Full move history:", move_history)
+    
 
 
 
